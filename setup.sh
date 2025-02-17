17f93888256bd7dccc9536246d99e9414aa4426e
dambergn <dambergn@gmail.com>
2025-02-17 12:48:38 -0800

    adding docker installer

--- setup.sh
+++ setup.sh
@@ -48,13 +48,12 @@ function check_docker_version {
         echo "Docker is not installed."
         # Install docker & docker compose
         sudo apt update
-        sudo apt install apt-transport-https ca-certificates curl software-properties-common
+        sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
         sudo apt-key fingerprint 0EBFCD88
         sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
         sudo apt update
-        sudo apt install docker-ce
-        sudo systemctl status docker
+        sudo apt install -y docker-ce
         sudo usermod -aG docker $USER
 
         # Create dockernet network
