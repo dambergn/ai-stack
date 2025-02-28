e3577a4fddbaf835b9e49b7505ce57dce7c83b0f
dambergn <dambergn@gmail.com>
2025-02-27 16:10:42 -0800

    fixing comfy setup

--- comfyui/setup_comfyui.sh
+++ comfyui/setup_comfyui.sh
@@ -10,16 +10,16 @@ RUN apt-get update && apt-get install -y git && apt-get clean
 
 ENV ROOT=/stable-diffusion
 RUN --mount=type=cache,target=/root/.cache/pip \\
-  git clone https://github.com/comfyanonymous/ComfyUI.git $\{ROOT\} && \\
+  git clone https://github.com/comfyanonymous/ComfyUI.git ${ROOT} && \\
   cd $\{ROOT\} && \\
   git checkout master && \\
   pip install -r requirements.txt
 
-WORKDIR $\{ROOT\}
+WORKDIR ${ROOT}
 COPY . /docker/
-RUN chmod u+x /docker/entrypoint.sh && cp /docker/extra_model_paths.yaml $\{ROOT\}
+RUN chmod u+x /docker/entrypoint.sh && cp /docker/extra_model_paths.yaml ${ROOT}
 
-ENV NVIDIA_VISIBLE_DEVICES=all PYTHONPATH="\${PYTHONPATH}:\${PWD}" CLI_ARGS=""
+ENV NVIDIA_VISIBLE_DEVICES=all PYTHONPATH=\"\${PYTHONPATH}:\${PWD}\" CLI_ARGS=\"\"
 EXPOSE 7860
-ENTRYPOINT ["/docker/entrypoint.sh"]
-CMD python -u main.py --listen --port 7860 $\{CLI_ARGS\}" > ./stable-diffusion-webui-docker/services/comfy/Dockerfile
+ENTRYPOINT [\"/docker/entrypoint.sh\"]
+CMD python -u main.py --listen --port 7860 ${CLI_ARGS}" > ./stable-diffusion-webui-docker/services/comfy/Dockerfile
