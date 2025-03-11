fc030c143a33796f22de445de31ba21eaf1dfe69
dambergn <dambergn@gmail.com>
2025-03-11 10:19:03 -0700

    setting up nginx server

--- nginx/setup_nginx.sh
+++ nginx/setup_nginx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir certs
+
 # Generate Self Assigned Cert
 openssl req -x509 -newkey rsa:4096 -keyout certs/localhost.key -out certs/localhost.crt -days 365 -nodes -subj "/C=US/ST=State/L=Locality/O=Organization/CN=localhost"
