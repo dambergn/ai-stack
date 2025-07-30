899ed6a15e8f6dd8abd81e4f6eacb716397279f3
dambergn <dambergn@gmail.com>
2025-07-29 17:52:31 -0700

    working on nginx config

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git pull
+CURRENT_DIR=$(pwd)
+
+git_pull_current_branch() {
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  git pull origin "$current_branch"
+}
+
+
 
 echo "Updating stackui..."
 cd "${CURRENT_DIR}/stackui"
@@ -11,3 +18,10 @@ echo "Updating nginx..."
 cd "${CURRENT_DIR}/nginx"
 sudo docker compose down
 sudo docker compose up --build --force-recreate -d
+
+
+main(){
+    git_pull_current_branch
+}
+
+main
