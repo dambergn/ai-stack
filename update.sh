ef3ab04998bfbd45b6358f05f6c8e3e41c740325
dambergn <dambergn@gmail.com>
2025-07-29 17:48:38 -0700

    working on nginx config

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+git pull
+
 echo "Updating stackui..."
 cd "${CURRENT_DIR}/stackui"
+sudo docker compose down
+sudo docker compose up --build --force-recreate -d
+
+echo "Updating nginx..."
+cd "${CURRENT_DIR}/nginx"
 sudo docker compose down
 sudo docker compose up --build --force-recreate -d
