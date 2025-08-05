bf485509bfcf0df1cc544df2b3efff5cee58cd4e
dambergn <dambergn@gmail.com>
2025-08-05 10:15:50 -0700

    updating ollama version

--- setup.sh
+++ setup.sh
@@ -85,7 +85,9 @@ IP_ADDRESS=$(ip route get 8.8.8.8 | awk '{print $7}')
 # Install functions
 install_portainer(){
     echo "Installing Portainer..."
-    sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.27.1
+    # sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.27.1
+    cd "${CURRENT_DIR}/portainer"
+    sudo docker compose up --build --force-recreate -d
 }
 
 install_stackui() {
