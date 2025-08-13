4a1cb7d6d0a6747d2580ef04cc60c831b8ecaeed
dambergn <dambergn@gmail.com>
2025-08-13 08:02:32 -0700

    updating update script

--- update.sh
+++ update.sh
@@ -17,6 +17,16 @@ update_services() {
     cd "${CURRENT_DIR}/nginx"
     sudo docker compose down
     sudo docker compose up --build --force-recreate -d
+
+    echo "Updating ollama..."
+    cd "${CURRENT_DIR}/ollama"
+    sudo docker compose down
+    sudo docker compose up --build --force-recreate -d
+
+    echo "Updating open-webui..."
+    cd "${CURRENT_DIR}/open-webui"
+    sudo docker compose down
+    sudo docker compose up --build --force-recreate -d
 }
 
 
